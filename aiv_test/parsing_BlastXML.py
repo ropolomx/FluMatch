f0379f27be35190e7538569343320693f6b727d7
Rodrigo O. Polo <r.ortegapolo@uleth.ca>
2016-05-31 18:14:04 -0600

    Changes to subprocesses

--- aiv_test/parsing_BlastXML.py
+++ aiv_test/parsing_BlastXML.py
@@ -1,68 +1,91 @@
 #!/usr/bin/env python
 
 ###################################################################
-##### Diagflu: a program to automate annotation and search of #####  
-############## the closest strain match of AIV CDS ################
+##### Diagflu: a script to automate annotation and search of #####  
+############## the closest strain match of AIV contigs ############
 ###################################################################
 
 from Bio import SeqIO
 from Bio.Blast import NCBIXML
+# from Bio.Blast.Applications import NcbiblastnCommandline
 import subprocess
+import re
 import os
 import argparse
-import sys
+import glob
 
 def arguments():
 
     parser = argparse.ArgumentParser() 
-    parser.add_argument('--blast-database', default = None, help = 'Path to database to BLAST your contigs against')
+
+    parser.add_argument('--blast-database', required = True, default = None, help = 'Path to database to BLAST your contigs against')
 
     parser.add_argument('contigs', help = 'FASTA formatted file with contig sequences')
 
-    # parser.add_argument('--top-results', default = None, help = 'Number of top BLAST hits to report for each contig')
+    parser.add_argument('--top-hits', required = True, default = None, help = 'Number of top BLAST hits to report for each contig')
+
+    parser.add_argument('--prokka-dir', help = 'Directory for prokka files')
 
     return parser.parse_args()
 
-def handle_input(filepath):
+def prokka_annotate(prokka_dir, fasta):
+
+    prokka = subprocess.call(['prokka', '--kingdom', 'Viruses',\
+            '--outdir', prokka_dir, fasta]) # outdir should be strain name or sequence run ID; also set prefix and locustag to strain name
+
+    return prokka 
+
+
+def get_top_blastn_hit(num_top_results, blastdb):
+
+    # prokka_ffn = glob.iglob('./prokka_annotation/*.ffn')
+    # os.walk
+    blastn = ('blastn',
+              '-query', prokka_dir,
+              '-db', blastdb,
+              '-outfmt', '5')
+
+    blastn_out = subprocess.check_output(blastn)
     
-    with open(filepath, 'r') as f:
-        read_data = f.read()
-        #os.listdir(
-    return read_data
+    results = NCBIXML.read(StringIO(blastn_out))
 
-def prokka_annotate(handle):
+    with open('TopBLASTHits.txt','w') as f:
 
-    return subprocess.call(['prokka', '--kingdom', 'Viruses', '--outdir',
-    'prokka_annotation_script', handle])
+        f.write('\t'.join(['Query','Strain','SubjStart','SubjEnd',\
+                'Identities','QueryCoverage','PercentID','Length',\
+                 'e-value']))
+        f.write('\n')
+        for res in results:
+            for aln in res.alignments[0:int(num_top_results)]:
+                for hsp in aln.hsps:
+                    res_query = res.query
+                    strain=aln.title
+                    strain=re.sub(r'gnl\|(\w*|\W*)\|\d*\s','',strain)
+                    sbjct_start=hsp.sbjct_start
+                    sbjct_end=hsp.sbjct_end
+                    identities=hsp.identities
+                    query_cover=(float(hsp.sbjct_end)-\
+                            float(hsp.sbjct_start-1))/\
+                            (float(hsp.query_end)-float(hsp.query_start\
+                            -1))*100
+                    identity_perc=float(hsp.identities)/\
+                            float(hsp.align_length)*100
+                    length=hsp.align_length
+                    e_value=hsp.expect
+        f.write('\t'.join([str(res_query),str(strain),str(sbjct_start),\
+                str(sbjct_end),str(identities),str(query_cover),\
+                str(identity_perc),str(e_value)]))
+        f.write('\n')
 
-#def blastn_top(contig):
+    report = f
 
-#def results():
-#
-# open("TopHitsFromBLASTXML.txt", "w") as f:
-#
-#    f.write("\t".join(["Query", "Strain", "SubjStart", "SubjEnd", "Identities", "QueryCoverage", "PercentID", "e-value"]))
-#    f.write("\n")
-#    for rec in NCBIXML.parse(open("flublast.xml")):
-#        for aln in rec.alignments[0:2]:
-#            for hsp in aln.hsps:
-#                rec_query = rec.query
-#                strain = aln.title
-#		strain = re.sub(r'gnl\|(\w*|\W*)\|\d*\s','',strain)
-#		sbjct_start = hsp.sbjct_start
-#		sbjct_end = hsp.sbjct_end
-#		identities = hsp.identities
-#		query_cover = (float(hsp.sbjct_end) - float(hsp.sbjct_start - 1))/(float(hsp.query_end)-float(hsp.query_start - 1))*100
-#		identity_perc = float(hsp.identities) / float(hsp.align_length) * 100
-#		e_value = hsp.expect
-#                f.write("\t".join([str(rec_query), str(strain), str(sbjct_start), str(sbjct_end), str(identities), str(query_cover), str(identity_perc), str(e_value)]))
-#                f.write("\n")
+    return report
 
 def main():
  
     args = arguments()
-    handle = handle_input(args.contigs)
-    annotations = prokka_annotate(handle)
+    annotations = prokka_annotate(args.prokka_dir, args.contigs)
+    top_hits = get_top_blastn_hit(args.top_hits, args.blast_database)
 
 if __name__ == '__main__':
     main()
